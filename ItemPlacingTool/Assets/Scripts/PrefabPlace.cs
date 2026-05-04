b1ff5ef16941042b25b0dee7ee0a8d05489c8125
Paula-LL <plizelucea@gmail.com>
2026-05-04 19:26:20 +0200

    Prefab Created

--- ItemPlacingTool/Assets/Scripts/PrefabPlace.cs
+++ ItemPlacingTool/Assets/Scripts/PrefabPlace.cs
@@ -1,9 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 
 public class PrefabPlace : Editor
 {
-    
+    GameObject lastInstantiateObject = null;
+
+    void CreateEmptyGameObjectInEditor() {
+        lastInstantiateObject = new GameObject("PersistentGameObject");
+        Undo.RegisterCreatedObjectUndo(lastInstantiateObject, "CreateEmptyGameObjectInEditor");
+    }
+
+    void DestroyLastPesistentGameObjectFromEditor() { 
+        if(lastInstantiateObject != null)
+        {
+            Undo.DestroyObjectImmediate(lastInstantiateObject);
+        }
+    }
+
+    void ChangeLastPersistentGameObjectName() { 
+        if (lastInstantiateObject != null)
+        {
+            Undo.RecordObject(lastInstantiateObject, "ChangeLastPersistentGameObjectName");
+            lastInstantiateObject.name += "Altered";
+        }
+    }
 }
